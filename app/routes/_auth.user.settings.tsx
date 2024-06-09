b8f2b7b3927833dc58e0f29fdac4bab61548354c
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2024-06-09 20:56:16 +0200

    Fix titles for Goals list and Settings

--- app/routes/_auth.user.settings.tsx
+++ app/routes/_auth.user.settings.tsx
@@ -1,3 +1,4 @@
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
 import { useTranslation } from 'react-i18next';
 
 import {
@@ -10,13 +11,28 @@ import {
 import { PageTitle } from '~/components/ui/page-title';
 import { PageContent } from '~/components/ui/page-content';
 import { ConfirmationForm } from '~/components/ui/confirmation-form';
+import i18next from '~/i18n.server';
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => [
+  {
+    title: data?.title || 'Financial Goals',
+  },
+];
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const t = await i18next.getFixedT(await i18next.getLocale(request));
+
+  return {
+    title: t('user-settings.title'),
+  };
+}
 
 export default function () {
   const { t } = useTranslation();
 
   return (
     <>
-      <PageTitle title={t('user-settings.title')} />
+      <PageTitle title={t('user-settings.page.title')} />
       <PageContent>
         <Card>
           <CardHeader>
